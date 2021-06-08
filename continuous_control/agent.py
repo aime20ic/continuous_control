fix PPO ratio computation error

continuous_control/agent.py
@@ -42,7 +42,7 @@ class Agent():
         # Set agent training hyperparameters
         self.batch_size = kwargs.get('batch_size', 64)          # minibatch size
         self.gamma = kwargs.get('gamma', 0.99)                  # discount factor
-        self.lr = kwargs.get('lr', 1e-4)                        # learning rate 
+        self.lr = kwargs.get('lr', 5e-4)                        # learning rate 
         self.optimizer = None
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -174,7 +174,7 @@ class Agent():
         window_avg = []
         window_std = []
         window = deque(maxlen=window_size)
-
+        
         # Create avg score
         avg = [np.mean(scores[:i+1]) for i in range(len(scores))]
         for i in range(len(scores)):

continuous_control/ppo.py
@@ -79,8 +79,8 @@ class PPO(Agent):
         new_probs = policy_dict['log_pi']
         entropy = -policy_dict['entropy']
             
-        # Compute weights ratio
-        ratio = new_probs / old_probs
+        # Compute weights ratio from log probabilities (ratio = new / old)
+        ratio = (new_probs - old_probs).exp()
             
         # clipped function
         clip = torch.clamp(ratio, 1-self.epsilon, 1+self.epsilon)
@@ -207,12 +207,11 @@ class PPO(Agent):
             # Get average reward
             total_rewards = np.sum(rewards, axis=0)
             score = np.mean(total_rewards)
-            scores.append(score)
 
             # Save most recent scores
             scores_window.append(score)
             scores.append(score)
-            
+
             # Calculate average & standard deviation of current scores
             scores_mean = np.mean(scores_window)
             scores_std = np.std(scores_window)